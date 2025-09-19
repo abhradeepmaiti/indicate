a14e0cce514276e05ec26cebf947d798d39fdd94
***** <721466+soodoku@users.noreply.github.com>
2025-09-18 21:45:30 -0700

    github pages python publish

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,15 +12,32 @@ sys.path.insert(1, os.path.abspath('../../'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'indicate'
-copyright = '2023, rajashekar chintalapati and gaurav sood'
-author = 'rajashekar chintalapati and gaurav sood'
-release = '0.3.0'
+copyright = '2024, Rajashekar Chintalapati and Gaurav Sood'
+author = 'Rajashekar Chintalapati and Gaurav Sood'
+release = '0.3.1'
+version = '0.3.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+]
+
 templates_path = ['_templates']
 exclude_patterns = []
+
+# -- Options for autodoc ----------------------------------------------------
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
